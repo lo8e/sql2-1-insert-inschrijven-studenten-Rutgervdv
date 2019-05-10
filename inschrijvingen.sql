1ae4b07e096847c58139c919c854180cf97d5209
Rutgervdv <47628982+Rutgervdv@users.noreply.github.com>
2019-05-10 12:19:46 +0200

    opdracht 3

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -13,3 +13,11 @@ VALUES
 	('90002', 'onfroy',"",'XXXtentacion', '23-01-1998', '2401er','florida','65','1.75','9-05-2019'),
     ('90003', 'skies',"",'lill', '04-08-1998', '2401rt','chambersburg','65','1.75','8-05-2019'),
     ('90004', 'wrld',"",'juice', '02-12-1998', '2304xc','chicago','60','1.83','7-05-2019');
+    
+    INSERT INTO klassen_studenten
+(ov_nummer, klas_code) 
+VALUES
+    (90001,'lo9e-amo1'),
+    (90002,'lo9e-amo2'),
+    (90003,'lo9e-amo3'),
+    (90004,'lo9e-amo3');
