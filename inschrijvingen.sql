013cd0408f382d2a933038b977c4a1ed262e738c
Rutgervdv <47628982+Rutgervdv@users.noreply.github.com>
2019-05-10 14:36:54 +0200

    opdracht 6

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -31,3 +31,14 @@ WHERE ov_nummer = '90002';
 UPDATE studenten
 SET actief = false
 WHERE inschrijvings_datum <= "1-1-2017";
+
+
+SELECT concat(studenten.voornaam,' ', studenten.tussenvoegsel,' ', studenten.achternaam) AS Naam, klassen.cohort , klassen.klas_code, klassen_studenten.ov_nummer, concat(docenten.voorletters, ' ', docenten.tussenvoegsel, ' ', docenten.achternaam) AS 'slb-er' FROM studenten
+INNER JOIN klassen_studenten 
+ON klassen_studenten.ov_nummer = studenten.ov_nummer
+INNER JOIN klassen
+ON klassen.klas_code = klassen_studenten.klas_code
+INNER JOIN docenten
+ON klassen.slb_code = docenten.docent_code
+WHERE studenten.actief = true
+ORDER BY  klassen.klas_code ASC, studenten.achternaam ASC;
